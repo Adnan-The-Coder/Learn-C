4fc7f035968bc6a504500f36b43e86f76d8c4c66
Adnan <adnanali23189@gmail.com>
2024-10-30 06:56:33 +0530

    Add Chapter 1

---
+++ chapter_1/01_variable.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int number; // this is stored in RAM 
+    number = 6;
+    printf("The Value of the number -> %d",number);
+
+    // Characters in C 
+    char a ;
+    a = 's';
+    return 0;
+}
---
+++ chapter_1/03_userInputs.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    int a ;
+    scanf("%d", &a); // & is amp. percent and it denotes the address of a here
+    printf("The value of a is %d",a);
+    return 0;
+}
